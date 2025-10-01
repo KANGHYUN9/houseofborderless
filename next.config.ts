3a7152057c11f2fa9dacb48eb3dc3f7af0035eda
brutaljaws <brutal.jaws@gmail.com>
2025-10-01 23:15:50 +0900

    Edit: image rules

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,11 @@ if (typeof process.getMaxListeners === "function" && typeof process.setMaxListen
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
+    images: {
+        deviceSizes: [320, 420, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 24, 32, 48, 64, 96, 128, 256, 384],
+        formats: ["image/webp", "image/avif"],
+    },
 };
 
 export default nextConfig;
